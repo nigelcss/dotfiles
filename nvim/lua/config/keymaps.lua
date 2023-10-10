7b628154ed390f2f2ffb5f71e565f24e40dbfed2
nigelcss <nigel@slack-smith.com>
2023-10-10 18:49:29 +1100

    Added key bindings for tmux nav and resizing

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,13 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>t", vim.cmd.UndotreeToggle)
+
+vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", vim.cmd.TmuxNavigateRight)
+
+vim.keymap.set("n", "<A-h>", vim.cmd.TmuxResizeLeft)
+vim.keymap.set("n", "<A-j>", vim.cmd.TmuxResizeDown)
+vim.keymap.set("n", "<A-k>", vim.cmd.TmuxResizeUp)
+vim.keymap.set("n", "<A-l>", vim.cmd.TmuxResizeRight)
