d7f84b346eb02d89ef11812ce46f13807da25871
nigelcss <nigel@slack-smith.com>
2023-10-10 19:08:29 +1100

    Updated resizing config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.tmux_resizer_resize_count = 3
+vim.g.tmux_resizer_vertical_resize_count = 6
