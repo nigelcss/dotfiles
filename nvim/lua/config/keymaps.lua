d6c109efe0d00c46128a74b6f3df53265d38d7ac
nigelcss <nigel@slack-smith.com>
2023-10-13 19:28:48 +1100

    Keymap changes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,19 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>n", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>n", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
 
-vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", vim.cmd.TmuxNavigateRight)
+vim.keymap.set("n", "<C-h>", vim.cmd.TmuxNavigateLeft, { desc = "Navigate Left" })
+vim.keymap.set("n", "<C-j>", vim.cmd.TmuxNavigateDown, { desc = "Navigate Down" })
+vim.keymap.set("n", "<C-k>", vim.cmd.TmuxNavigateUp, { desc = "Navigate Up" })
+vim.keymap.set("n", "<C-l>", vim.cmd.TmuxNavigateRight, { desc = "Navigate Right" })
 
-vim.keymap.set({ "n", "i", "v" }, "<A-h>", vim.cmd.TmuxResizeLeft)
-vim.keymap.set({ "n", "i", "v" }, "<A-j>", vim.cmd.TmuxResizeDown)
-vim.keymap.set({ "n", "i", "v" }, "<A-k>", vim.cmd.TmuxResizeUp)
-vim.keymap.set({ "n", "i", "v" }, "<A-l>", vim.cmd.TmuxResizeRight)
+vim.keymap.set({ "n", "i", "v" }, "<A-h>", vim.cmd.TmuxResizeLeft, { desc = "Resize Left" })
+vim.keymap.set({ "n", "i", "v" }, "<A-j>", vim.cmd.TmuxResizeDown, { desc = "Resize Down" })
+vim.keymap.set({ "n", "i", "v" }, "<A-k>", vim.cmd.TmuxResizeUp, { desc = "Resize Up" })
+vim.keymap.set({ "n", "i", "v" }, "<A-l>", vim.cmd.TmuxResizeRight, { desc = "Resize Right" })
 
-vim.keymap.set("n", "<C-A-h>", vim.cmd.DapStepOut)
-vim.keymap.set("n", "<C-A-j>", vim.cmd.DapStepOver)
-vim.keymap.set("n", "<C-A-k>", vim.cmd.DapContinue)
-vim.keymap.set("n", "<C-A-l>", vim.cmd.DapStepInto)
+vim.keymap.set("n", "<C-A-h>", vim.cmd.DapStepOut, { desc = "Step Out" })
+vim.keymap.set("n", "<C-A-j>", vim.cmd.DapStepOver, { desc = "Step Over" })
+vim.keymap.set("n", "<C-A-k>", vim.cmd.DapContinue, { desc = "Continue" })
+vim.keymap.set("n", "<C-A-l>", vim.cmd.DapStepInto, { desc = "Step Into" })
