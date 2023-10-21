69723f03cfc7ac572f91e6e1dabe055c3968b65b
nigelcss <nigel@slack-smith.com>
2023-10-21 18:08:25 +1100

    Debug and test updates

--- nvim/lua/plugins/dap-go.lua
+++ nvim/lua/plugins/dap-go.lua
@@ -1,8 +1,12 @@
 return {
   {
     "leoluz/nvim-dap-go",
-    delve = {
-      build_flags = "-tags=test",
-    },
+    config = function()
+      require("dap-go").setup({
+        delve = {
+          build_flags = "-tags=test",
+        },
+      })
+    end,
   },
 }
---
+++ nvim/lua/plugins/lsp-go.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = false,
+                nilness = true,
+                unusedparams = false,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              buildFlags = { "-tags=test" },
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
