41ed0ae1a00aa74b94e80fac99988b94b7a9ac31
nigelcss <nigel@slack-smith.com>
2023-11-04 10:51:52 +1100

    Turned line col off for LSP gr

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,7 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>" }
+      keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "References" }
     end,
   },
 }
