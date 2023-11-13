a0245a937b0a86b9011e18e89c7c141e10b3e317
nigelcss <nigel@slack-smith.com>
2023-11-13 19:32:52 +1100

    updated wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,8 +11,6 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night Moon"
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
