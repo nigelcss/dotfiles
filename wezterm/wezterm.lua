b2e779d33c8b0ccf5903c4d8489985faa4660fde
nigelcss <nigel@slack-smith.com>
2023-11-13 19:29:33 +1100

    updated wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,7 +13,7 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Tokyo Night"
+config.color_scheme = "Tokyo Night Moon"
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 14
