2af7f0527790473e000dbe3af408c22f3d847018
nigelcss <nigel@slack-smith.com>
2023-12-12 22:39:40 +1100

    wezterm colorscheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = "Gruvbox"
+config.color_scheme = "Gruvbox dark, medium (base16)"
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Regular" })
 config.font_size = 14
