22aa86d100344ceca57ec73e7bb92bd452a01c16
nigelcss <nigel@slack-smith.com>
2023-12-14 12:10:47 +1100

    eslint formatting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,4 +7,18 @@ return {
       keys[#keys + 1] = { "gd", "<cmd>Telescope lsp_definitions show_line=false<cr>", desc = "Goto Definition" }
     end,
   },
+  opts = {
+    servers = { eslint = {} },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
 }
