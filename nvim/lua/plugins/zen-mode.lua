cb6fa4dcee4c0ec6247cb7f78d56535f6f0c2c12
nigelcss <nigel@slack-smith.com>
2024-01-02 16:31:56 +1100

    Config fix

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -1,3 +1,10 @@
 return {
   "folke/zen-mode.nvim",
+  plugins = {
+    tmux = { enabled = true },
+    wezterm = {
+      enabled = true,
+      font = "+1", -- (10% increase per step)
+    },
+  },
 }
