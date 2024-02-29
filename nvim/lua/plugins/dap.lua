38b3d5c32a9d5eb05cabe4153102bf1ea475e527
nigelcss <nigel@slack-smith.com>
2024-02-29 11:18:25 +1100

    Switched local launch config path

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -4,7 +4,7 @@ return {
     {
       "<leader>dc",
       function()
-        require("dap.ext.vscode").load_launchjs(".nvim/launch.json")
+        require("dap.ext.vscode").load_launchjs(".config/launch.json")
         require("dap").continue()
       end,
       desc = "Continue",
