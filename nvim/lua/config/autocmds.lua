5d02aad603788725b26127cd917e34a47e454759
nigelcss <nigel@slack-smith.com>
2024-03-13 17:59:19 +1100

    quickfix list nav

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.cmd([[
+  augroup QuickFixKeyMappings
+    autocmd!
+    autocmd FileType qf nnoremap <buffer> <Tab> :cnewer<CR>
+    autocmd FileType qf nnoremap <buffer> <S-Tab> :colder<CR>
+  augroup END
+]])
